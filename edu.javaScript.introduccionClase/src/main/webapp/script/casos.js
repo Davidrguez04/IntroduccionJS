aa3f566305a1f838b5f1aac51207f63e1387766b
Davidrguez04 <122359982+Davidrguez04@users.noreply.github.com>
2023-01-23 10:10:33 +0100

    Tipos Casos

---
+++ edu.javaScript.introduccionClase/src/main/webapp/script/casos.js
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+//typeof
+function typeeof(){
+     let variable1 = 10;
+     let variable2 = 5.3;
+
+      document.write(typeof(variable1));
+      document.write(typeof variable2);
+      }
+
+
+//boolean
+function boolean(){
+     let es = true;
+     document.write(typeof(es));
+     document.write(es);
+     }
+
+//number
+function number(){
+     let variable1 = 10;
+     let variable2 = 5.3;
+
+      document.write(typeof(variable1));
+      document.write(typeof variable2);
+      }
+
+//bigint
+function bigint(){
+     let numeroGrande = 4567891324657987654;
+     let numeroGrandeCasteado = BigInt(987654321654987654);
+
+     document.write(typeof(numeroGrande));
+     document.write(numeroGrande);
+     document.write(typeof(numeroGrandeCasteado));
+    document.write(numeroGrandeCasteado);
+    }
+//string
+function string(){
+
+     let texto = "Soy un string";     
+
+    document.write(typeof(texto));
+
+     document.write(texto);
+     }
+
+
+
+
+
+//undefined
+function undefined(){
+     let variableVacia;
+     document.write(variableVacia);
+
+     document.write(typeof(variableVacia));
+     }
