3b0a267a681ebf1460eea89831c1c286b51a99c4
Davidrguez04 <122359982+Davidrguez04@users.noreply.github.com>
2023-01-30 09:22:54 +0100

    Metodo CharAt()

---
+++ edu.javaScript.introduccionClase/src/main/webapp/script/metodocharAt.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+function metodoCharAt(){
+	var cualquierCadena="Brave new world";
+
+document.write("El carácter en el índice 0 es '" + cualquierCadena.charAt(0) + "'");
+document.write("El carácter en el índice 1 es '" + cualquierCadena.charAt(1) + "'");
+document.write("El carácter en el índice 2 es '" + cualquierCadena.charAt(2) + "'");
+document.write("El carácter en el índice 3 es '" + cualquierCadena.charAt(3) + "'");
+document.write("El carácter en el índice 4 es '" + cualquierCadena.charAt(4) + "'");
+document.write("El carácter en el índice 999 es '" + cualquierCadena.charAt(999) + "'");
+}
